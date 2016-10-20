a7a24a1dc825b9f01f4fbbc7654deb96289b35b5
shijiem <sma.mashijie@gmail.com>
2016-10-20 22:07:51 +0200

    add

--- game.js
+++ game.js
@@ -390,7 +390,7 @@ Ship = function () {
                                0, 11,
                                3,  6]);
 
-  this.delayBeforeBullet = 0;
+  this.delayBeforeBullet = 10;
 
   this.postMove = this.wrapPostMove;
 
